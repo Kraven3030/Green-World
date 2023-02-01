ca535cb94cc68fe31275313f005a4f33f2f2de6f
BlakeR <blakereed3030@gmail.com>
2023-02-01 12:15:07 -0800

    signup route created

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -6,9 +6,22 @@ const router = express.Router();
 const passport = require('../config/passport')
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const db = require("../models");
 const User = db.User;
 
-
+router.post('/signup', async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
+        const newUser = new User({
+            username: req.body.username,
+            password: hashedPassword
+        });
+        await newUser.save();
+        res.send({ message: 'User created successfully' });
+    } catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
 
 
 module.exports = router
