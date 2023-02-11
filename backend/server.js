9eb83b8e42b85e32ba033bfcc185ae98e028e9db
BlakeR <blakereed3030@gmail.com>
2023-02-11 11:56:47 -0800

    updates to user schema

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const methodOverride = require('method-override');
 const cors = require('cors');
+const bodyParser = require("body-parser")
+const cookieParser = require("cookie-parser")
+
 // Access to controllers
 const usersCtrl = require('./controllers/users.js')
 
@@ -19,8 +22,32 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(methodOverride('_method'));
+app.use(bodyParser.json())
+app.use(cookieParser(process.env.COOKIE_SECRET))
 
+//Add the client URL to the CORS policy
 
+const whitelist = process.env.WHITELISTED_DOMAINS
+    ? process.env.WHITELISTED_DOMAINS.split(",")
+    : []
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    },
+
+    credentials: true,
+}
+
+app.use(cors(corsOptions))
+
+app.get("/", function (req, res) {
+    res.send({ status: "success" })
+})
 
 // +-+-+-+-+-+-+
 // |R|O|U|T|E|S|
