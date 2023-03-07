447f2ba9db4d80a84d00d4d0cfa9e6382ae56e52
BlakeR <blakereed3030@gmail.com>
2023-03-07 10:11:07 -0800

    frontend connected to backend

--- frontend/green-world/src/App.js
+++ frontend/green-world/src/App.js
@@ -1,8 +1,23 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 
+// Imports for components and pages
+import Login from './pages/Login/login'
+
 function App() {
+
+
+
   return (
-    <h1>Hello World</h1>
+    <>
+      <header>
+        <h1>This is my header</h1>
+      </header>
+      <Routes>
+        <Route path='/' element={<Login />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ frontend/green-world/src/pages/Login/login.js
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { userLogin } from '../../utils/api'
+
+function Login() {
+    const [loginForm, setLoginForm] = useState({
+        username: '',
+        password: ''
+    })
+
+
+
+    return (
+        <div></div>
+    )
+}
+
+export default Login
